6877506ae1275db76543157e5d166d74ae226375
adinaamza <amza_adina@yahoo.com>
2021-03-22 11:02:52 +0100

    Task 2. (before the Saturday mail)

--- src/app/shared/services/asset.service.ts
+++ src/app/shared/services/asset.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core'
 import { Observable, of } from 'rxjs'
 import { Asset } from '../models/asset.model'
 import { mockAssetHttpResponse } from './asset.test'
-import { delay, tap } from 'rxjs/operators'
+import { delay, tap, catchError } from 'rxjs/operators'
 import { HttpClient } from '@angular/common/http';
 import { getRandomInt } from '../functions'
+import { AlertController } from '@ionic/angular'
 
 @Injectable({
   providedIn: 'root',
 })
 export class AssetService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+    private alertController: AlertController) {
   }
 
   getAll(): Observable<Asset[]> {
@@ -26,7 +28,34 @@ export class AssetService {
       tap(() => { // a small chance for the data fetch to error
         if (getRandomInt(10) % 10 === 0) throw Error('Http error')
       }),
+      catchError(this.handleError<Asset[]>())
     )
+    
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      this.presentAlert();
+      return of(result as T);
+    };
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Something went wrong.',
+      message: 'Please reload and try again.',
+      buttons: [{
+        text: 'Reload',
+        role: 'cancel',
+        handler: () => {
+          //should find a more elegant solution to reload the component itself
+          //not the entire page
+          window.location.reload()
+        }
+      }]
+    });
+
+    await alert.present();
   }
 }
 
