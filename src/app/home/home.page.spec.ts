73ceb55340e8f9bf7c1ac1abc928c37c7c56ad9d
adinaamza <amza_adina@yahoo.com>
2021-03-22 16:31:40 +0100

    Task 3. Not finished. An error I cannot find prevents the Home test results from being shown (including console).

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,24 +1,44 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { Router } from '@angular/router'
+import { RouterTestingModule } from '@angular/router/testing'
 import { IonicModule } from '@ionic/angular'
+import { AssetCardComponent } from './asset-card/asset-card.component'
 
 import { HomePage } from './home.page'
+import { Component } from '@angular/core'
+import { mockAssets } from '../shared/services/asset.test'
 
+@Component({
+  template: ` <app-home></app-home>`,
+})
+class TestHostComponent {
+  assets = mockAssets
+  loaded = true
+}
 describe('HomePage', () => {
-  let component: HomePage
-  let fixture: ComponentFixture<HomePage>
+  let component: TestHostComponent
+  let fixture: ComponentFixture<TestHostComponent>
 
   beforeEach(waitForAsync(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      declarations: [HomePage, AssetCardComponent, TestHostComponent],
+      imports: [IonicModule.forRoot(), HttpClientTestingModule, RouterTestingModule]
     }).compileComponents()
 
-    fixture = TestBed.createComponent(HomePage)
+    fixture = TestBed.createComponent(TestHostComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
   }))
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should have an assets list', async () => {
+    // fixture.detectChanges()
+    expect(component.assets).toBeTruthy
+    console.log(component.assets)
+
   })
 })
--- src/app/shared/services/asset.service.spec.ts
+++ src/app/shared/services/asset.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AssetService } from './asset.service'
 
@@ -6,7 +7,10 @@ describe('AssetService', () => {
   let service: AssetService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
+      providers: [AssetService]
+    })
     service = TestBed.inject(AssetService)
   })
 
