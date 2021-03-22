5012c0d83ced6c6c515f01fa2bd4daa2a2339f4f
adinaamza <amza_adina@yahoo.com>
2021-03-22 11:00:11 +0100

    Task 1. (before the Saturday mail)

--- src/app/shared/services/asset.service.ts
+++ src/app/shared/services/asset.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs'
 import { Asset } from '../models/asset.model'
 import { mockAssetHttpResponse } from './asset.test'
 import { delay, tap } from 'rxjs/operators'
+import { HttpClient } from '@angular/common/http';
 import { getRandomInt } from '../functions'
 
 @Injectable({
@@ -10,11 +11,17 @@ import { getRandomInt } from '../functions'
 })
 export class AssetService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   getAll(): Observable<Asset[]> {
-    return of(mockAssetHttpResponse).pipe(
+    // return this.http.get<Asset[]>('api/assets').pipe( 
+    // is the clean solution when we do API calls, but I did not manage to fake a 
+    // url to call :D
+
+    // this is the fix when we know we want only the data from the response
+    // to process in an observable
+    return of(mockAssetHttpResponse.data).pipe(
       delay(getRandomInt(1000) + 500), // fake random http delay,
       tap(() => { // a small chance for the data fetch to error
         if (getRandomInt(10) % 10 === 0) throw Error('Http error')
